8117cb4886fc926e289a36ba8c60f89a606d72e6
Hayoung-Im <u1402474@umail.utah.edu>
2025-02-22 14:43:19 -0700

    update hal_gpio.c

--- Src/hal_gpio.c
+++ Src/hal_gpio.c
@@ -3,9 +3,12 @@
 #include <stm32f0xx_hal_gpio.h>
 #include "main.h"
 
+void My_HAL_RCC_GPIOA_CLK_ENABLE(void) {
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+}
+
 void My_HAL_RCC_GPIOC_CLK_ENABLE(void) {
     RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
-
 }
 
 
@@ -41,22 +44,10 @@ void MY_HAL_GPIO_Init(GPIO_TypeDef *GPIOx, uint32_t PinMask, uint32_t Mode, uint
     }
 }
 
-
-
-
-
-/*
-void My_HAL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin)
-{
-}
-*/
-
-
 GPIO_PinState My_HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
     return (GPIOx->IDR & GPIO_Pin) ? GPIO_PIN_SET : GPIO_PIN_RESET;
 }
-
 
 void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
 {
@@ -71,4 +62,3 @@ void My_HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
     GPIOx->ODR ^= GPIO_Pin;
 }
-
